7061765834b97b55b36f8787c38547b870982e57
Daniel Bojdo <daniel@bojdo.eu>
2016-03-21 11:12:35 +0000

    [upd] vendor option / values collection serialisation

--- src/WebitShipmentBundle.php
+++ src/WebitShipmentBundle.php
@@ -6,11 +6,15 @@
  
 namespace Webit\Bundle\ShipmentBundle;
 
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Types\Type;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Webit\Bundle\ShipmentBundle\DependencyInjection\Compiler\ResolveTargetEntityPass;
 use Webit\Bundle\ShipmentBundle\DependencyInjection\Compiler\SerializerPass;
 use Webit\Bundle\ShipmentBundle\DependencyInjection\Compiler\VendorAdapterPass;
+use Webit\Shipment\DBAL\VendorOptionCollectionType;
+use Webit\Shipment\DBAL\VendorOptionValueCollectionType;
 
 /**
  * Class WebitShipmentBundle
@@ -18,11 +22,36 @@ use Webit\Bundle\ShipmentBundle\DependencyInjection\Compiler\VendorAdapterPass;
  */
 class WebitShipmentBundle extends Bundle
 {
+    /**
+     * @var array
+     */
+    private static $dbalTypes = array(
+        VendorOptionCollectionType::TYPE_NAME => 'Webit\Shipment\DBAL\VendorOptionCollectionType',
+        VendorOptionValueCollectionType::TYPE_NAME => 'Webit\Shipment\DBAL\VendorOptionValueCollectionType'
+    );
+
     public function build(ContainerBuilder $container) {
+        $this->registerDoctrineDBALTypes();
+
         parent::build($container);
 
         $container->addCompilerPass(new ResolveTargetEntityPass('webit_shipment.entity_map'));
         $container->addCompilerPass(new VendorAdapterPass());
         $container->addCompilerPass(new SerializerPass());
+    }
+
+    public function boot()
+    {
+        $this->registerDoctrineDBALTypes();
+    }
+
+    private function registerDoctrineDBALTypes()
+    {
+        foreach (self::$dbalTypes as $typeName => $class) {
+            try {
+                Type::addType($typeName, $class);
+            } catch (DBALException $e) {
+            }
+        }
     }
 }
